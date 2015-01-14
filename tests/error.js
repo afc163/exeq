cb706fe099adabffff2d66a41a8de0fb29de313b
afc163 <afc163@gmail.com>
2015-01-14 20:17:50 +0800

    fix error in travis

--- tests/error.js
+++ tests/error.js
@@ -6,7 +6,7 @@ test('catch error', function(t) {
   exeq('not-existed-cmd').then(function(results) {
   }).catch(function(err) {
     t.equal(err.code, 127);
-    t.ok(err.stderr.indexOf('command not found') > -1);
+    t.ok(err.stderr.indexOf('not found') > -1);
   }).finally(function() {
     t.end();
   });
